460c8e029aeed7a8af5acfa239f2c4871cc1f772
Horace He <chilli@fb.com>
2022-05-24 15:45:06 -0700

    fix some more stuff

--- src/extension.ts
+++ src/extension.ts
@@ -7,23 +7,22 @@ import * as cp from 'child_process';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pytorch-aws-scratch-helper" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pytorch-aws-scratch-helper.allocateServer', async () => {
+	context.subscriptions.push(vscode.commands.registerCommand('pytorch-aws-scratch-helper.allocateServer', () => {
 		const terminal = vscode.window.createTerminal("KEEP OPEN - Salloc-ing scratch");
 		terminal.show(true);
-		const serverNumber = 27;
+		const serverNumber = 49;
 		terminal.sendText(`ssh -t f392b7dc-4dec-47a7-8887-9bcd206c4a79 '/opt/slurm/bin/salloc -p dev -G 1 -t 5:00:00 -w a100-st-p4d24xlarge-${serverNumber} --exclusive'`);
 		cp.execSync(`echo ${serverNumber} > ~/.ssh/ai_ssh/$USER/number.txt`);
-	});
+	}));
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('pytorch-aws-scratch-helper.initialSetup', async () => {
+		const terminal = vscode.window.createTerminal("Run commands");
+		terminal.show(true);
+		terminal.sendText('echo 5');
+	}));
 }
 
 // this method is called when your extension is deactivated
