1fb501d5855747d283d855733a7a62fb8b5fe54f
Horace He <chilli@fb.com>
2022-05-24 17:15:27 -0700

    Add ability to select server

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,31 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
+import * as fs from 'fs';
+import * as process from 'process';
 
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	context.subscriptions.push(vscode.commands.registerCommand('pytorch-aws-scratch-helper.allocateServer', () => {
-		const terminal = vscode.window.createTerminal("KEEP OPEN - Salloc-ing scratch");
+	context.subscriptions.push(vscode.commands.registerCommand('pytorch-aws-scratch-helper.allocateServer', async () => {
+		const path = `${process.env.HOME!.trim()}/.ssh/ai_ssh/${process.env.USER!.trim()}/number.txt`;
+		let previous_val = undefined;
+		if (fs.existsSync(path)) {
+			previous_val = fs.readFileSync(path).toString();
+		}
+		const searchQuery = await vscode.window.showInputBox({
+			value: previous_val,
+			prompt: "Enter the number of the compute node you want to use",
+		  });
+		if (searchQuery === undefined) {
+			throw "Didn't provide a number";
+		}
+
+		const terminal = vscode.window.createTerminal("Salloc-ing scratch");
 		terminal.show(true);
-		const serverNumber = 49;
-		terminal.sendText(`ssh -t f392b7dc-4dec-47a7-8887-9bcd206c4a79 '/opt/slurm/bin/salloc -p dev -G 1 -t 5:00:00 -w a100-st-p4d24xlarge-${serverNumber} --exclusive'`);
+		const serverNumber = parseInt(searchQuery);
+		terminal.sendText(`ssh -t f392b7dc-4dec-47a7-8887-9bcd206c4a79 'tmux new -d "/opt/slurm/bin/salloc -p dev -G 1 -t 5:00:00 -w a100-st-p4d24xlarge-${serverNumber} --exclusive"'`);
 		cp.execSync(`echo ${serverNumber} > ~/.ssh/ai_ssh/$USER/number.txt`);
 	}));
 
